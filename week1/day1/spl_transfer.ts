ffabf99f37ac4e637f007f58c5fbfaf5a03c9b9e
Darker98 <zmsiddiqui321@gmail.com>
2025-07-02 22:29:55 +0300

    Added SPL token code

---
+++ week1/day1/spl_transfer.ts
@@ -0,0 +1,34 @@
+import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import wallet from "./Turbin3-wallet.json"
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { get } from "http";
+
+// We're going to import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+// Mint address
+const mint = new PublicKey("F5jQH6a8W5j6VVDDF8pQUGe3kFJaEo8xciZGbuk2j7H5");
+
+// Recipient address
+const to = new PublicKey("2QkJLTKTtYFHS6xir1TEXLSdajM7r1Djf96JogKnRGSR");
+
+(async () => {
+    try {
+        // Get the token account of the fromWallet address, and if it does not exist, create it
+        const ataFrom = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
+
+        // Get the token account of the toWallet address, and if it does not exist, create it
+        const ataTo = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
+
+        // Transfer the new token to the "toTokenAccount" we just created
+        const transfer_tx = await transfer(connection, keypair, ataFrom.address, ataTo.address, keypair.publicKey, 1_000_000n);
+
+        console.log("Tx ID: ", transfer_tx);
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
